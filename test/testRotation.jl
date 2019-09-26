0b4bc3d7d260d1df08d523d369fb99554f9b9f82
Eran Treister <erantreister@gmail.com>
2019-09-26 13:18:44 +0300

    update to travis

--- test/testRotation.jl
+++ test/testRotation.jl
@@ -10,8 +10,6 @@ if plotting
 	close("all");
 end
 
-#export getDiamondModel,getBallModel ;
-
 function getDiamondModel(n::Array{Int64})
 n_tup = tuple(n...);
 n = collect(n_tup);
@@ -21,19 +19,6 @@ I = (abs.(X)+abs.(Y)+abs.(Z)) .<= div(n[1],4);
 u[I] .= 1.0;
 return u;
 end
-
-function getBallModel(n::Array{Int64})
-n_tup = tuple(n...);
-n = collect(n_tup);
-u = zeros(Float32,n_tup);
-X,Y,Z = meshgrid(-div(n[1],2):div(n[1],2),-div(n[2],2):div(n[2],2),-div(n[3],2):div(n[3],2))
-I = (sqrt(X.^2+Y.^2 + Z.^2)) .<= div(n[1],4);
-u[I] .= 1.0;
-return u;
-end
-
-
-
 
 println("Rotation operator transpose test");
 n = [33,33,33];
